0bd48f5b3306dadafd6b993761444b1deb8b64a9
Stephen Korzenstein <skorzenstein@gmail.com>
2022-05-10 12:38:01 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -21,15 +21,15 @@ function App() {
   const [eventType, setEventType] = useState('Sports')
 
 
-  const dateFunction = (date) => {
-    const altDate = date.toISOString();
-    const newDate = altDate.replace(/[/]/g, "-");
-    const shortDate = newDate.substring(0,newDate.indexOf("T"));
-    const finalDate = `${shortDate}T23:00:00Z`; 
-    return finalDate;
-  }
-  const todaysDate = dateFunction(new Date());
-  console.log(dateFunction(dateValue));
+  // const dateFunction = (date) => {
+  //   const altDate = date.toISOString();
+  //   const newDate = altDate.replace(/[/]/g, "-");
+  //   const shortDate = newDate.substring(0,newDate.indexOf("T"));
+  //   const finalDate = `${shortDate}T23:00:00Z`; 
+  //   return finalDate;
+  // }
+  // const todaysDate = dateFunction(new Date());
+  // console.log(dateFunction(dateValue));
 
   const handleSubmit = (event) => {
     event.preventDefault();
