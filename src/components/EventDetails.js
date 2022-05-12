963383a49e43103aa541135f8f4c20290f3afcc0
Stephen Korzenstein <skorzenstein@gmail.com>
2022-05-12 09:51:54 -0400

    styled event details page and added some logic to form

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -3,18 +3,17 @@ import { useState, useEffect } from "react";
 
 import axios from "axios";
 import firebase from "./firebase";
-import { getDatabase, ref, push, onValue } from 'firebase/database';
-
+import { getDatabase, ref, push, onValue } from "firebase/database";
 
 const EventDetails = () => {
-  const [userInput, setUserInput] = useState('');
+  const [userInput, setUserInput] = useState("");
   // const [newUrl, setNewUrl] = useState('')
-  const [firedata, setFiredata] = useState([])
+  const [firedata, setFiredata] = useState([]);
   const { eventID } = useParams();
   const [detailsArray, setDetailsArray] = useState({ loading: false });
-  const [showButton, setShowButton] = useState(false)
+  const [showButton, setShowButton] = useState(false);
 
-// Ticketmaster API Call for Individual Event 
+  // Ticketmaster API Call for Individual Event
   useEffect(() => {
     const configDetails = {
       method: "get",
@@ -44,97 +43,102 @@ const EventDetails = () => {
         // pushing the values from the object into our emptryArray
         emptyArray.push({ personalID: key, name: data[key] });
       }
-      const updatedArray = emptyArray.filter(item => item.name.id === eventID)
+      const updatedArray = emptyArray.filter(
+        (item) => item.name.id === eventID
+      );
 
-      if(updatedArray.length === 0 ) {
-        setShowButton(false)
+      if (updatedArray.length === 0) {
+        setShowButton(false);
       } else {
-        setShowButton(true)
+        setShowButton(true);
       }
-      setFiredata(updatedArray)
-    }); 
+      setFiredata(updatedArray);
+    });
   }, [eventID]);
 
   const handleInputChange = (event) => {
-  setUserInput(event.target.value);
-}
+    setUserInput(event.target.value);
+  };
 
-// Form submission that will store our new firebase data on submit
-const handleSubmit = (event) => {
+  // Form submission that will store our new firebase data on submit
+  const handleSubmit = (event) => {
     event.preventDefault();
     // create a reference to our database
     const database = getDatabase(firebase);
     const dbRef = ref(database);
-    // push the value of the `userInput` state to the database
-    // let removeWhite = userInput.trim().split(/[\s,\t,\n]+/).join(' ');
-    // let addHyphen = removeWhite.replace(/ /g, "-")
-    // let removeAppost = addHyphen.replaceAll("'", "")
-    // let lowered = removeAppost.toLowerCase()
-    const uniqueInput = {
-      userInput, id: eventID, 
-      title: detailsArray.name, 
-      img: detailsArray.images[1].url, 
-      start: detailsArray.dates.start.localDate, 
-      time: detailsArray.dates.start.localTime, 
-      // priceMax: detailsArray.priceRanges[0].max, 
-      // priceMin: detailsArray.priceRanges[0].min, 
-      tickets: detailsArray.url, 
-      address: detailsArray._embedded.venues[0].address.line1, 
-      city: detailsArray._embedded.venues[0].city.name, 
-      latitude: detailsArray._embedded.venues[0].location.latitude, 
-      longitude: detailsArray._embedded.venues[0].location.longitude,
-      venue: detailsArray._embedded.venues[0].name,
-      attendees: [
-        userInput
-      ]
+    // push the value of the `userInput` state to db
+
+    if (userInput === null) {
+      alert("Please enter your host name");
+    } else {
+      const uniqueInput = {
+        userInput,
+        id: eventID,
+        title: detailsArray.name,
+        img: detailsArray.images[1].url,
+        start: detailsArray.dates.start.localDate,
+        time: detailsArray.dates.start.localTime,
+        // priceMax: detailsArray.priceRanges[0].max,
+        // priceMin: detailsArray.priceRanges[0].min,
+        tickets: detailsArray.url,
+        address: detailsArray._embedded.venues[0].address.line1,
+        city: detailsArray._embedded.venues[0].city.name,
+        latitude: detailsArray._embedded.venues[0].location.latitude,
+        longitude: detailsArray._embedded.venues[0].location.longitude,
+        venue: detailsArray._embedded.venues[0].name,
+        attendees: [userInput],
+      };
+      console.log(uniqueInput);
+      push(dbRef, uniqueInput);
+      setUserInput("");
     }
-    console.log(uniqueInput)
-    push(dbRef, uniqueInput);
-    setUserInput('');
-  }
+  };
 
   return (
-    <div>
+    <div className="eventDetails">
       {detailsArray.loading ? (
-        <div>
-          <div>
-            <h2>{detailsArray.name}</h2>
-            {/* <h3>{detailsArray.classifications[0].genre.name}</h3> */}
-            <p>Placeholder</p>
+        <div className="wrapper">
+          <div className="title">
+          <div class="img">
+            <img src={detailsArray.images[0].url} alt={detailsArray.name} />
           </div>
-          <div>
-            <img src={detailsArray.images[1].url} alt={`Placeholder`} />
+          <div className="titleBottom">
+            
+            <div className="titleText">
+               <h2>{detailsArray.name}</h2>
+                <h5>
+                  {detailsArray.dates.start.localTime} {detailsArray.dates.start.localDate}
+                </h5>
+                <p>
+                  {detailsArray._embedded.venues[0].city.name}
+                </p>
+              </div>
           </div>
-          {
-              showButton ?
-              <Link to={`/personal/${firedata[0].personalID}`}>
-                Link
-                </Link>
-              : (
+          {showButton ? (
+            <Link
+              className="buttonLink"
+              to={`/personal/${firedata[0].personalID}`}
+            >
+              Your Event
+            </Link>
+          ) : (
             <form action="submit">
-            <label htmlFor="newLiked">Add an event to your list</label>
-            <input 
-            type="text" 
-            id="newLiked" 
-            onChange={handleInputChange}
-            value={userInput}
-            />
-            <button onClick={handleSubmit}>Like Event</button>
-          </form>)
-            }
-
-          
+              <label htmlFor="newLiked">Enter Host Name</label>
+              <input
+                className="hostInput"
+                type="text"
+                id="newLiked"
+                onChange={handleInputChange}
+                value={userInput}
+              />
+              <button className="createEventButton" onClick={handleSubmit}>
+                Create Event
+              </button>
+            </form>
+          )}
+          </div>
         </div>
       ) : null}
-
-        {/* <div>
-      {firedata === eventID ? (
-        <div>
-        <a href={newUrl}>Link</a>
-        </div>
-        )
-        : null }
-      </div> */}
     </div>
   );
 };
