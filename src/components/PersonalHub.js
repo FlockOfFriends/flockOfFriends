b0afd202068f148c9a054559714e4aef3ff5120d
mzmarlies <marliesclaire@gmail.com>
2022-05-14 18:36:08 -0600

    changing layout of the personalEventHub

--- src/components/PersonalHub.js
+++ src/components/PersonalHub.js
@@ -32,34 +32,17 @@ const PersonalHub = () => {
           {liked.map((like) => {
             return (
                 <li key={like.personalID}>
-                  <h4>{like.name.title}</h4>
-                  <p>Make This Your Event!</p>
                   <div className="eventContent">
                     <Link to={`/personal/${like.personalID}`}>
                       <div className="personalEventImg">
                         <img src={like.name.img} alt={`Placeholder`} />
                       </div>
+                      <div className="hubText">
+                        <h4>{like.name.title}</h4>
+                        <button>See Event</button>
+                        <button>Delete Event</button>
+                      </div>  
                     </Link>
-                    <div className="hubText">
-                      <form action="submit">
-                        <label htmlFor="newEvent" className="sr-only">Make This Your Event</label>
-                        <input 
-                          type="text" 
-                          id="newEvent" 
-                          placeholder="Event Name"
-                        />
-                        <label htmlFor="eventDescription"className="sr-only">Type a message</label>
-                        <textarea 
-                          name="eventDescription" 
-                          id="eventDescription" 
-                          cols="30" 
-                          rows="5"
-                          placeholder="Write your event description here so you and your flock can get excited!">
-                        </textarea>
-                        {/* need to add an onSubmit function to the button */}
-                        <button>Create Event</button>
-                      </form>
-                    </div>  
                   </div>
                 </li>
             );
